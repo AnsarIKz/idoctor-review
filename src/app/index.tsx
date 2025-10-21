b89c6b61fce4d651029c56785060448e3c089f59
Ansar <nansarik@mail.ru>
2025-10-21 12:35:42 +0600

    refactor: changed entrypoint source to ./SRC/APP

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,2 +1,3 @@
+export * from "./layouts/RootLayout";
 export * from "./metrics/YandexMetrics";
 export * from "./providers/WithProviders";
--- src/app/layouts/RootLayout.tsx
+++ src/app/layouts/RootLayout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { BookingSheet, BookingModal } from "@/features/book-appointment";
-import { Header } from "@/widgets";
-import { Providers } from "@/app/providers/WithProviders";
+import { Header } from "@/widgets/header";
+import { Providers } from "@/app";
 import "@/app/styles/globals.css";
 
 const geistSans = Geist({
